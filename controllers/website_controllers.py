0a5871b6f99e78db45f285c2a2cbbdb5c6b7c0bf
shadman_git <khezri.shadman@gmail.com>
2025-05-28 17:03:35 +0330

    update templates views

--- controllers/website_controllers.py
+++ controllers/website_controllers.py
@@ -5,13 +5,14 @@ from odoo.http import request
 class ShopController(http.Controller):
 
 #----------------------
+# controller for product list
     @http.route('/shop/products', type='http', auth='public', website=True)
     def product_list(self, **kwargs):
         products = request.env['mini_store.product'].search([])
         return request.render('mini_store.product_list_template', {'products': products})
     
 #----------------------
-
+# controller for product detail
     @http.route('/shop/product/<int:product_id>', type='http', auth='public', website=True)
     def product_detail(self, product_id, **kwargs):
         product = request.env['mini_store.product'].browse(product_id)
@@ -20,7 +21,7 @@ class ShopController(http.Controller):
         return request.render('mini_store.product_detail_template', {'product': product})
     
 #----------------------
-
+# controller for submit_order
     @http.route('/shop/submit_order', type='http', auth='public', website=True, methods=['POST'])
     def submit_order(self, **post):
         customer_id = int(post.get('customer_id'))
@@ -41,7 +42,7 @@ class ShopController(http.Controller):
         return request.redirect(f'/payment/verify/{order.id}')
     
 #----------------------
-
+# controller for invoice_detail
     @http.route('/shop/invoice/<int:invoice_id>', type='http', auth='public', website=True)
     def invoice_detail(self, invoice_id, **kwargs):
         invoice = request.env['mini_store.invoice'].browse(invoice_id)
@@ -50,6 +51,7 @@ class ShopController(http.Controller):
         return request.render('mini_store.invoice_template', {'invoice': invoice})
     
 #----------------------
+# controller for product_list_cached
     @http.route('/shop/products/cached', type='http', auth='public', website=True)
     def product_list_cached(self, **kwargs):
         products = request.env['mini_store.product'].search([])
